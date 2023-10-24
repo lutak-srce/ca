98c798c215a4f3fd2d616496087a9b9ef322f1d1
Jakov Sosic <jsosic@gmail.com>
2023-10-24 22:22:58 +0200

    Add support for content

--- manifests/trust/add.pp
+++ manifests/trust/add.pp
@@ -2,18 +2,30 @@
 # = Define: ca::trust::add
 #
 define ca::trust::add (
-  $source,
-  $ensure = file,
+  $source  = '',
+  $content = '',
+  $ensure  = file,
 ) {
 
   include ::ca::trust
+
+  $final_source = $content ? {
+    /^$/    => $source,
+    default => undef,
+  }
+
+  $final_content = $content ? {
+    /^$/    => undef,
+    default => $content,
+  }
 
   file { "${::ca::params::cert_addon_dir}/${name}":
     ensure  => $ensure,
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => $source,
+    source  => $final_source,
+    content => $final_content,
     require => Exec['enable_ca_trust'],
     notify  => Exec['update_ca_trust'],
   }
